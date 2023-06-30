cc090ef34a3f07d03d8782538ecb89be01327f2d
Matheus <matheushdealmeida@gmail.com>
2023-06-30 15:00:00 -0300

    Initial commit

---
+++ automotive/src/main/java/br/com/mha/checklistauto/ui/SplashActivity.kt
@@ -0,0 +1,13 @@
+package br.com.mha.checklistauto.ui
+
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.mha.checklistauto.R
+
+class SplashActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
