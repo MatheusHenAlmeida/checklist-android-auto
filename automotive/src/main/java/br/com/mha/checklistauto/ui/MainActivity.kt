f62db6cf9c953ab9f37db7908a226308e597cfec
Matheus <matheushdealmeida@gmail.com>
2023-06-30 15:56:13 -0300

    Configured View Binding on Main Screen

--- automotive/src/main/java/br/com/mha/checklistauto/ui/MainActivity.kt
+++ automotive/src/main/java/br/com/mha/checklistauto/ui/MainActivity.kt
@@ -3,14 +3,23 @@ package br.com.mha.checklistauto.ui
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import br.com.mha.checklistauto.R
+import br.com.mha.checklistauto.databinding.ActivityMainBinding
 
 class MainActivity : Activity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setupAddListButtonAction()
+        setContentView(binding.root)
+    }
+
+    private fun setupAddListButtonAction() {
+        binding.btAddList.setOnClickListener {
+            // TODO: Show dialog to insert a new list
+        }
     }
 
     companion object {
